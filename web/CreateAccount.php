ad12f7db59c4ad24c2f645b9ff7996a59c0d2563
Mhivelin <marius.hivelin@yahoo.fr>
2023-10-29 00:40:53 +0200

    test ajout

--- web/CreateAccount.php
+++ web/CreateAccount.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création de compte bibydex</title>
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -25,7 +26,7 @@
             <!-- body de la zone -->
             <div class="card-body">
                 <!-- formulaire pour obtenir les données pour créer un compte : méthode POST-->
-                <form action="assets/scripts/CreateAccount.php" method="post">
+                <form action="assets/db/CreateAccount.php" method="post">
 
                     <!-- pseudo -->
                     <label for="pseudo" class="form-label">Pseudo</label>
