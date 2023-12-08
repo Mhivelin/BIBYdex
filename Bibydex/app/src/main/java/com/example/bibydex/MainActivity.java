8733aff01868d1d16ee23c8f7046d594a7465eee
Cam17O <pauget.camille17@gmail.com>
2023-12-08 17:51:19 +0100

    menu

--- Bibydex/app/src/main/java/com/example/bibydex/MainActivity.java
+++ Bibydex/app/src/main/java/com/example/bibydex/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.bibydex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View Toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((androidx.appcompat.widget.Toolbar) Toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
     }
 }
